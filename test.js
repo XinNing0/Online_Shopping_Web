517fe51add18b1206db4b6fb4317d05906a2bd50
XinNing0 <71576483+XinNing0@users.noreply.github.com>
2024-08-02 22:03:56 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+const imageContentArrays = {
+    "WOMEN" :
+        {"imageUrl": "https://images.lululemon.com/is/image/lululemon/NA_Jun24_Wk3_W_Skirts_Ecomm_MegaNavPromo_SummerClothes_02?$promoBlock$&wid=768&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+            "titleContent": "On the same page sa summer",
+            "description": "These things keep skirt tight and easy, no matter what the itenary",
+            "shopContent": "Shop Skirts"
+        },
+
+    "MEN" : {
+        "imageUrl": "https://images.lululemon.com/is/image/lululemon/na_Jun24_wk1_M_Golf_MegaNavPromo_Shirts?$promoBlock$&wid=768&op_usm=0.8,1,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72",
+        "titleContent": "Top of your game.",
+        "description": "The same page sa summer",
+        "shopContent": "Top of your game."
+    }}
+
+console.log(imageContentArrays["WOMEN"].imageUrl)
